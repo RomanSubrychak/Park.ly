82617f85555637b1bc9cf9d4bf7c8c4064633655
Roman Subrychak <rsubrichak@yahoo.com>
2017-12-25 09:46:50 -0600

    shadow added to RoundedButton

--- Park.ly/View/RoundButton.swift
+++ Park.ly/View/RoundButton.swift
@@ -11,6 +11,10 @@ import UIKit
 class RoundButton: UIButton {
 	
 	override func awakeFromNib() {
-		self.layer.cornerRadius = self.frame.height / 2
+		self.layer.cornerRadius = self.frame.width / 2
+		
+		self.layer.shadowRadius = 20
+		self.layer.shadowOpacity = 0.5
+		self.layer.shadowColor = UIColor.black.cgColor
 	}
 }
